1e7f2924b65eaee7921fea816dc4af069bbe2259
Jean Ramos <i1415905@continental.edu.pe>
2018-12-15 23:39:57 -0500

    Variables de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -13,7 +13,7 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://coffe-user:123masgo456@ds125479.mlab.com:25479/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
